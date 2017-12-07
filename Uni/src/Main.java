43256dc87ef582bea887105e8fca2b4bd9b3b16a
Thorben Grosse <Thorben.Grosse@rwe.com>
2017-12-07 08:44:11 +0100

    #1

---
+++ Uni/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Awesome mystic uni stuff incoming... soon... maybe...
+        System.out.println("Hello World!");
+    }
+}

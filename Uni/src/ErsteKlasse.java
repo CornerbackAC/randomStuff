3c5a187e256c524c38862a9b427c8deb335b082d
Thorben Grosse <Thorben.Grosse@rwe.com>
2017-12-08 15:52:40 +0100

    zip

---
+++ Uni/src/ErsteKlasse.java
@@ -0,0 +1,6 @@
+public class ErsteKlasse {                          // 1
+    public static void main(String[] args) {        // 2
+// Gibt eine Begruessung am Bildschirm aus
+        System.out.println("Herzlich willkommen!"); // 3
+    }                                               // 4
+}                                                   //5
---
+++ Uni/src/Testklasse.java
@@ -0,0 +1,11 @@
+public class Testklasse { // 1
+    public static void main(String[] args) { // 2
+        // Breite und Laenge eines Rechtecks
+        int breite = 1200; // 3
+        int laenge = 27; // 4
+        // Berechnung der Flaeche
+        int flaeche = breite * laenge; // 5
+        // Ausgabe der Flaeche
+        System.out.println(17 * 2 % - (4 - 11) / (-3 + 5)); // 6 Seite 81 Script 2
+    } // 7
+} // 8
